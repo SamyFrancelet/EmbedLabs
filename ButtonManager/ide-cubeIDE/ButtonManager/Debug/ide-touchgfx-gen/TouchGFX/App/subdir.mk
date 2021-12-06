9866c19637d77cfba90d2f9c1da3824e794d03c3
Francelet Samy <samy.francelet@ik.me>
2021-12-06 21:08:58 +0100

    Added result screenshot

--- ButtonManager/ide-cubeIDE/ButtonManager/Debug/ide-touchgfx-gen/TouchGFX/App/subdir.mk
+++ ButtonManager/ide-cubeIDE/ButtonManager/Debug/ide-touchgfx-gen/TouchGFX/App/subdir.mk
@@ -5,7 +5,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-D:/work/EmbedLabs/ButtonManager/ide-touchgfx-gen/TouchGFX/App/app_touchgfx.c 
+C:/work/EmbedLabs/ButtonManager/ide-touchgfx-gen/TouchGFX/App/app_touchgfx.c 
 
 C_DEPS += \
 ./ide-touchgfx-gen/TouchGFX/App/app_touchgfx.d 
@@ -15,6 +15,6 @@ OBJS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-ide-touchgfx-gen/TouchGFX/App/app_touchgfx.o: D:/work/EmbedLabs/ButtonManager/ide-touchgfx-gen/TouchGFX/App/app_touchgfx.c ide-touchgfx-gen/TouchGFX/App/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F746xx -c -I"D:/work/EmbedLabs/ButtonManager/ide-cubeIDE/ButtonManager/Core/Inc" -I../Drivers/STM32F7xx_HAL_Driver/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F7xx/Include -I../Drivers/CMSIS/Include -I"D:/work/EmbedLabs/ButtonManager/ide-cubeIDE/ButtonManager" -I"D:/work/EmbedLabs/ButtonManager/ide-cubeIDE/ButtonManager/../../src" -I"D:/work/EmbedLabs/ButtonManager/ide-cubeIDE/ButtonManager/../../src/xf/include" -I"D:/work/EmbedLabs/ButtonManager/ide-cubeIDE/ButtonManager/../../src/xf/port/" -I"D:/work/EmbedLabs/ButtonManager/ide-touchgfx-gen/TouchGFX/target" -I"D:/work/EmbedLabs/ButtonManager/ide-touchgfx-gen/TouchGFX/target/generated" -I"D:/work/EmbedLabs/ButtonManager/ide-touchgfx-gen/Middlewares/ST/touchgfx/framework/include" -I"D:/work/EmbedLabs/ButtonManager/ide-touchgfx-gen/TouchGFX/App" -Og -ffunction-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+ide-touchgfx-gen/TouchGFX/App/app_touchgfx.o: C:/work/EmbedLabs/ButtonManager/ide-touchgfx-gen/TouchGFX/App/app_touchgfx.c ide-touchgfx-gen/TouchGFX/App/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F746xx -c -I"C:/work/EmbedLabs/ButtonManager/ide-cubeIDE/ButtonManager/Core/Inc" -I../Drivers/STM32F7xx_HAL_Driver/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F7xx/Include -I../Drivers/CMSIS/Include -I"C:/work/EmbedLabs/ButtonManager/ide-cubeIDE/ButtonManager" -I"C:/work/EmbedLabs/ButtonManager/ide-cubeIDE/ButtonManager/../../src" -I"C:/work/EmbedLabs/ButtonManager/ide-cubeIDE/ButtonManager/../../src/xf/include" -I"C:/work/EmbedLabs/ButtonManager/ide-cubeIDE/ButtonManager/../../src/xf/port/" -I"C:/work/EmbedLabs/ButtonManager/ide-touchgfx-gen/TouchGFX/target" -I"C:/work/EmbedLabs/ButtonManager/ide-touchgfx-gen/TouchGFX/target/generated" -I"C:/work/EmbedLabs/ButtonManager/ide-touchgfx-gen/Middlewares/ST/touchgfx/framework/include" -I"C:/work/EmbedLabs/ButtonManager/ide-touchgfx-gen/TouchGFX/App" -Og -ffunction-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
